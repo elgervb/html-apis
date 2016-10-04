9ea273f487ee72676b47501f5180b5a9c1612229
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-10-04 23:04:07 +0200

    add marker & info window

--- src/js/map/map.js
+++ src/js/map/map.js
@@ -5,12 +5,35 @@ export default class Map {
         const here = new google.maps.LatLng(51.846, 5.86);
 
         /* eslint-disable no-new */
-        new google.maps.Map(document.getElementById('map'), {
+        const map = new google.maps.Map(document.getElementById('map'), {
             center: here,
-            zoom: 15,
+            zoom: 12,
             disableDefaultUI: true,
         });
         /* eslint-enable no-new */
+        const marker = this.createMarker(here, map);
+        const info = this.createInfoWindow(here);
+        marker.addListener('click', () => {
+            info.open(map);
+        });
+    }
+
+    createMarker(position, map) {
+        const marker = new google.maps.Marker({
+            position,
+            map,
+            title: 'You are here',
+        });
+
+        return marker;
+    }
+
+    createInfoWindow(position) {
+        const coordInfoWindow = new google.maps.InfoWindow();
+        coordInfoWindow.setContent('<h1>You are here</h1>');
+        coordInfoWindow.setPosition(position);
+
+        return coordInfoWindow;
     }
 
     load(apiKey, callback) {
