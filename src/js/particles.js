08e0a928f9f404f3deae009570366ce3a6eed84a
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-10-07 14:02:19 +0200

    Refactored Canvas

--- src/js/particles.js
+++ src/js/particles.js
@@ -1,73 +1,18 @@
 import Settings from './particles/particlesettings';
+import Canvas from './particles/canvas';
 import Particle from './particles/particle';
 import {} from './particles/particles.scss';
 
 const settings = new Settings();
 
-const Canvas = function drawCanvas() {
-    let ctx;
-    const particles = [];
+// dynamically create the canvas
+const c = document.createElement('canvas');
+document.body.appendChild(c);
 
+const canvas = new Canvas(c, settings);
+canvas.draw();
 
-    function add(particle) {
-        console.log(`add particle nr ${particles.length}`);
-        particles.push(particle);
-    }
-
-    function create() {
-        // dynamically create the canvas
-        const c = document.createElement('canvas');
-
-        ctx = c.getContext('2d');
-        c.width = settings.width;
-        c.height = settings.height;
-        c.style.backgroundColor = 'blue';
-        c.style.opacity = 1;
-        c.style.position = 'fixed';
-        c.style.left = 0;
-        c.style.top = 0;
-        c.id = 'particles';
-        document.body.appendChild(c);
-    }
-
-    function draw() {
-        let i;
-
-        ctx.globalCompositeOperation = 'source-over';
-        ctx.fillStyle = `rgba(52, 55, 60, ${settings.trail})`;
-        ctx.fillRect(0, 0, settings.width, settings.height);
-        ctx.globalCompositeOperation = 'lighter';
-
-        for (i = 0; i < particles.length; i++) {
-            particles[i].draw(ctx);
-        }
-    }
-
-    function remove() {
-        particles.pop();
-    }
-
-    for (let i = 0; i < settings.number; i++) {
-        /* eslint-disable new-cap */
-        add(new Particle(settings));
-    }
-
-
-    return {
-        add,
-        create,
-        draw,
-        remove,
-    };
-};
-
-const canvas = Canvas();
-canvas.create();
-// update
-setInterval(canvas.draw, 33);
-
-
-// UI
+// Event listeners
 document.querySelector('.btn-add-particle').addEventListener('mouseup', () => {
     canvas.add(new Particle(settings));
 });
---
+++ src/js/particles/canvas.js
@@ -0,0 +1,52 @@
+import Particle from './particle';
+
+export default class Canvas {
+    constructor(canvasEl, settings) {
+        this.settings = settings;
+        this.particles = [];
+        this.ctx = canvasEl.getContext('2d');
+        this.drawInterval = false;
+        this._init(canvasEl, settings);
+    }
+
+    add(particle) {
+        /* eslint-disable no-console */
+        console.log(`add particle nr ${this.particles.length + 1}`);
+        /* eslint-enable no-console */
+        this.particles.push(particle);
+    }
+
+    draw() {
+        if (!this.drawInterval) {
+            this.drawInterval = setInterval(this.draw.bind(this), 33);
+        }
+        let i;
+        this.ctx.globalCompositeOperation = 'source-over';
+        this.ctx.fillStyle = `rgba(52, 55, 60, ${this.settings.trail})`;
+        this.ctx.fillRect(0, 0, this.settings.width, this.settings.height);
+        this.ctx.globalCompositeOperation = 'lighter';
+
+        for (i = 0; i < this.particles.length; i++) {
+            this.particles[i].draw(this.ctx);
+        }
+    }
+
+    remove() {
+        this.particles.pop();
+    }
+
+    _init(canvasEl, settings) {
+        canvasEl.width = settings.width;
+        canvasEl.height = settings.height;
+        canvasEl.style.backgroundColor = 'blue';
+        canvasEl.style.opacity = 1;
+        canvasEl.style.position = 'fixed';
+        canvasEl.style.left = 0;
+        canvasEl.style.top = 0;
+        canvasEl.id = 'particles';
+
+        for (let i = 0; i < settings.number; i++) {
+            this.add(new Particle(settings));
+        }
+    }
+}
